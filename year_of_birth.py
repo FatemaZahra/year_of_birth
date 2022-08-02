3e88e4a6e44d56d43e66a901543797cf6dbb9977
Fatema Zahra <fatema.zahra1312@gmail.com>
2022-08-02 16:46:19 +0100

    first commit

---
+++ year_of_birth.py
@@ -0,0 +1,23 @@
+def year_of_birth():
+    name = input("Please enter your name")
+    age = int(input("Please enter your age"))
+    year = 2022 - age
+    print(f"OMG ,{name} you are {age} years old so you were born in {year}")
+
+year_of_birth()
+
+full_name = input("Please enter your name")
+age = int(input("Please enter your age"))
+full_name= "Fatema Zahra"
+age = 25
+month = int(input("Please enter the month of birth as digit"))
+if month < 8:
+    print("Birthday has passed")
+elif month > 8:
+    print("Birthday yet to come")
+else:
+    date = int(input("Please enter date of birth"))
+    if date >=2:
+        print("Birthday has passed")
+    else:
+        print("Birthday yet to come")
